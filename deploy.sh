c782a1a2343f27807c7a6b4667f85bacf8993d3a
JP <jpsala@gmail.com>
2020-05-08 21:38:54 -0300

    deploy de iae.v2 a iae.admin

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ tar --create --bzip --verbose --file dist.bz2 dist/
 # > /dev/null 2>&1
 echo scp
 scp dist.bz2 iae.dyndns.org:/tmp
-echo ssh 'rm -rf /prg/iae.v2/dist/* ; tar -jxvf /tmp/dist.bz2 -C /prg/iae.v2 > /dev/null 2>&1 ; chmod 755 /prg/iae.v2/dist -R'
-ssh iae.dyndns.org 'rm -rf /prg/iae.v2/dist/* ; tar -jxvf /tmp/dist.bz2 -C /prg/iae.v2 > /dev/null 2>&1 ; chmod 755 /prg/iae.v2/dist -R'
+echo ssh 'rm -rf /prg/iae.admin/dist/* ; tar -jxvf /tmp/dist.bz2 -C /prg/iae.admin > /dev/null 2>&1 ; chmod 755 /prg/iae.admin/dist -R'
+ssh iae.dyndns.org 'rm -rf /prg/iae.admin/dist/* ; tar -jxvf /tmp/dist.bz2 -C /prg/iae.admin > /dev/null 2>&1 ; chmod 755 /prg/iae.admin/dist -R'
 rm -f dist.bz2
 echo Listo
